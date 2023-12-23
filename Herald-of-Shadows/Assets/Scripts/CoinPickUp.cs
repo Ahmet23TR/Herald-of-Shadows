42f6098c7eadfc23a154572f3b75fdcdbc9582aa
Ahmet Emin <gucluahmetemin@gmail.com>
2023-12-23 19:20:11 +0300

    Coin SFX added

--- Herald-of-Shadows/Assets/Scripts/CoinPickUp.cs
+++ Herald-of-Shadows/Assets/Scripts/CoinPickUp.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class CoinPickUp : MonoBehaviour
 {
+    [SerializeField] AudioClip coinPickupSfx;
     void OnTriggerEnter2D(Collider2D other) 
     {
         if(other.gameObject.tag == "Player")
         {
+            AudioSource.PlayClipAtPoint(coinPickupSfx, Camera.main.transform.position);
             Destroy(gameObject);
         }    
     }
